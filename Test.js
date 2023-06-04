38dea5a0ae50ae896554ef674219bdbf2adf7b7a
NotSpinZ <135542881+NotSpinZ@users.noreply.github.com>
2023-06-04 10:48:04 -0700

    Add files via upload

---
+++ Test.js
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <title>Random Move</title>
+  <style>
+    .blocklyBlockBody.randomMove {
+      background-color: #7fc97f;
+      color: white;
+    }
+  </style>
+</head>
+<body></body>
+</html>
